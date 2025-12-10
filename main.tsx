80dea675aa2d528cd5651afedc6188121a35be7e
idkjustarandomdudeherenothingtosee <promcpegamer99@gmail.com>
2025-12-10 11:53:59 +0300

    fix: make assets relative and use HashRouter for SPA routing

--- main.tsx
+++ main.tsx
@@ -1,5 +1,18 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById('root')
+if (!container) {
+  throw new Error('Failed to find root element with id="root"')
+}
+
+createRoot(container).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+)
